4e400c0bad5ac57dfdda4b1cdb36d30856de53aa
Jonathan Bahena <bahena@Jonathans-MacBook-Air.local>
2021-06-18 10:58:11 -0500

    finished product

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,11 @@
 class MoviesController < ApplicationController
+  def see_all_movies_method
+    render json: Movie.all
+  end
+  def see_one_movie_method
+    render json: Movie.first
+  end
+  def query_docs_method
+    render json: Movie.where
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  
+  get "/see_all_movies_route",controller: "movies",action: "see_all_movies_method"
+  get "/see_one_movie_route",controller: "movies",action: "see_one_movie_method"
+  get "/query_docs",controller: "movies",action: "query_docs_method"
+
 end
+
